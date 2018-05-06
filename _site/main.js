e414189a09a29600adaef715d67735d3e64774fc
lilygoose89 <32081648+lilygoose89@users.noreply.github.com>
2018-05-06 17:10:48 +0700

    fixed modal js
    now "dynamic" and uses class to call the matching modal instead of needing js for each id

--- _site/main.js
+++ _site/main.js
@@ -1,69 +1,64 @@
 $(document).ready(function(){
+  //click anywhere on the modal div (which includes the overlay) to exit the modal
+  $(".modal").click(function(){
+    $(".modal").removeClass("show");
+  });
+  //click on .button-service and call the modal that matches the id
+  $(".button-service").click(function(){
+    var id = $(this).attr("id");
 
-$("#trans").click(function(){
-  $("#transModal").addClass("show");
-});
-$("#admin").click(function(){
-  $("#adminModal").addClass("show");
-});
-$("#setup").click(function(){
-  $("#setupModal").addClass("show");
-});
-$("#sm").click(function(){
-  $("#smModal").addClass("show");
-});
-$(".modal").click(function(){
-  $(".modal").removeClass("show");
-});
+    $(".modal#"+id+"Modal").addClass("show");
+
+  });
 
 
-// Assign flexslider function to class .flexslider
-$('.flexslider').flexslider({
+  // Assign flexslider function to class .flexslider
+  $('.flexslider').flexslider({
 
-  // Customize flexslider properties
-  animation: "slide",
-  slideshowSpeed: 5000,
-  animationSpeed: 1000,
-  slideshow: false,
-  directionNav: false,
-  controlNav: false,
-  start: function (slider) {
+    // Customize flexslider properties
+    animation: "slide",
+    slideshowSpeed: 5000,
+    animationSpeed: 1000,
+    slideshow: false,
+    directionNav: false,
+    controlNav: false,
+    start: function (slider) {
 
-    // Click on the left arrow
-    $('#flex-left').on('click', function(event){
+      // Click on the left arrow
+      $('#flex-left').on('click', function(event){
 
-      // prevent default action
-      event.preventDefault();
+        // prevent default action
+        event.preventDefault();
 
-      // Move to previous slide
-      $('.flexslider').flexslider('prev');
+        // Move to previous slide
+        $('.flexslider').flexslider('prev');
 
-    });
+      });
 
-    // Click on the right arrow
-    $('#flex-right').on('click', function(event){
+      // Click on the right arrow
+      $('#flex-right').on('click', function(event){
 
-      // prevent default action
-      event.preventDefault();
+        // prevent default action
+        event.preventDefault();
 
-      // Move to next slide
-      $('.flexslider').flexslider('next');
+        // Move to next slide
+        $('.flexslider').flexslider('next');
 
-    });
+      });
 
-  }
+    }
 
-});
+  });
 
-//smooth scrolling better because rachel
-$("a[href^='#']").click(function () {
-  var link = this.hash.substr(1);
-  var section = $('*[id=' + link + ']');
+  //smooth scrolling better because rachel
+  $("a[href^='#']").click(function () {
+    var link = this.hash.substr(1);
+    var section = $('*[id=' + link + ']');
 
-  $('html,body').animate({
-    scrollTop: section.offset().top
-  }, 800);
-  return false;
-});
+    $('html,body').animate({
+      scrollTop: section.offset().top
+    }, 800);
+    return false;
+  });
 
 });
