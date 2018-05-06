024c89fb4ba7f1ae4add15c244418e17914f1540
lilygoose89 <32081648+lilygoose89@users.noreply.github.com>
2018-05-06 16:56:32 +0700

    fixed smooth scrolling
    wasn't working bc of / in href

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 $("#trans").click(function(){
   $("#transModal").addClass("show");
 });
@@ -53,39 +55,18 @@ $('.flexslider').flexslider({
 
 });
 
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
+//smooth scrolling better because rachel
+$("a[href^='#']").click(function () {
+
+
+  var link = this.hash.substr(1);
+  var section = $('*[id=' + link + ']');
+
+  $('html,body').animate({
+    scrollTop: section.offset().top
+  }, 800);
+  return false;
+});
+
+
+});
